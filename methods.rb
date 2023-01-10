f6c162d63c5e36c79b70e0c127e401b858e3b007
Mordechai <mordwein77@gmail.com>
2023-01-10 15:04:33 -0500

    commit tree 6941901b778f15f98a16b8505a3adfc83a923122

--- methods.rb
+++ methods.rb
@@ -1,1 +1,50 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name) 
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!" 
+end
+
+greet_with_default("Jack")
+
+def add(num1, num2)
+    num1 + num2
+end
+
+add(1,2)
+
+def halve(num1)
+    if num1.class != Integer
+        return nil
+    end
+    num1 / 2
+ end
+
+ halve(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+  
